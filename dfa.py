77723d7fea81569585a4d275e2c012ccaad46f77
Sidd <35938702+siddsp02@users.noreply.github.com>
2023-01-01 23:44:30 -0700

    Added lambda transition cycle detection.
    This helps avoid a case where lambda transitions back to the same states loop infinitely.

--- dfa.py
+++ dfa.py
@@ -64,7 +64,11 @@ class NFA(DFA):
             # process.
             if first:
                 state = init
+                seen = set()
                 while state in lambda_trans:
+                    if state in seen:
+                        break
+                    seen.add(state)
                     state = lambda_trans[state]
                     new_state |= {state}
                 queue.appendleft(new_state)
@@ -79,36 +83,49 @@ class NFA(DFA):
                 sym_state = frozenset()
                 for state in new_state:
                     trans = self.transfunc[state].get(sym)
-                    if trans is not None:
-                        if isinstance(trans, str):
-                            # Check if any other "sub-states" are reachable using lambda transitions.
-                            if trans in lambda_trans:
-                                s = trans
-                                trans = frozenset({trans})
-                                while s in lambda_trans:
-                                    s = lambda_trans[s]
-                                    trans |= {s}
-                        else:
-                            # Check if any other "sub-states" are reachable using lambda transitions
-                            # for each state in the trans set.
-                            for s in trans:
-                                if s in lambda_trans:
-                                    r = s
-                                    while r in lambda_trans:
-                                        r = lambda_trans[r]
-                                        trans |= {r}
-                        sym_state |= {trans} if isinstance(trans, str) else trans
-                        for s in sym_state:
-                            r = s
-                            while r in lambda_trans:
-                                sym_state |= {r}
-                                r = lambda_trans[r]
+                    if trans is None:
+                        continue
+                    if isinstance(trans, str):
+                        # Check if any other "sub-states" are reachable using lambda transitions.
+                        if trans in lambda_trans:
+                            s = trans
+                            trans = frozenset({trans})
+                            seen = set()
+                            while s in lambda_trans:
+                                if s in seen:
+                                    break
+                                seen.add(s)
+                                s = lambda_trans[s]
+                                trans |= {s}
+                    else:
+                        # Check if any other "sub-states" are reachable using lambda transitions
+                        # for each state in the trans set.
+                        for s in trans:
+                            if s in lambda_trans:
+                                r = s
+                                seen = set()
+                                while r in lambda_trans:
+                                    if r in seen:
+                                        break
+                                    seen.add(r)
+                                    r = lambda_trans[r]
+                                    trans |= {r}
+                    sym_state |= {trans} if isinstance(trans, str) else trans
+                    for s in sym_state:
+                        r = s
+                        seen = set()
+                        while r in lambda_trans:
+                            if r in seen:
+                                break
+                            seen.add(r)
+                            r = lambda_trans[r]
+                            sym_state |= {r}
                 sym_map[sym] = sym_state
             new_trans[new_state] = sym_map
             for sym in self.alphabet:
                 queue.appendleft(new_trans[new_state][sym])
             visited.add(new_state)
-        df = DFA(
+        ret = DFA(
             self.states.copy(),
             self.alphabet.copy(),
             new_trans,
@@ -117,8 +134,8 @@ class NFA(DFA):
         )
         for state in new_trans:
             if state & self.accepting:
-                df.accepting.add(state)
-        return df
+                ret.accepting.add(state)
+        return ret
 
     to_dfa = powerset_construct
 
@@ -131,33 +148,44 @@ class NFA(DFA):
 
 
 if __name__ == "__main__":
-    a = NFA(
-        states={"q0", "q1", "q2", "q3"},
-        alphabet={"0", "1"},
-        transfunc={
-            "q0": {"": "q2", "0": "q1"},
-            "q1": {"1": frozenset({"q1", "q3"})},
-            "q2": {"": "q1", "0": "q3"},
-            "q3": {"0": "q2"},
-        },
-        initial="q0",
-        accepting={"q2", "q3"},
-    )
-
-    b = NFA(
-        states={"q0", "q1", "q2", "q3"},
-        alphabet={"a", "b"},
-        transfunc={
-            "q0": {"": "q1"},
-            "q1": {"a": frozenset({"q1", "q2"}), "b": "q2"},
-            "q2": {"a": frozenset({"q0", "q2"}), "b": "q3"},
-            "q3": {"b": "q1"},
-        },
-        initial="q0",
-        accepting={"q0"},
-    )
-    x = a.to_dfa()
-    pprint(x)
-    print()
-    y = b.to_dfa()
-    pprint(y)
+    nfas = [
+        NFA(
+            states={"q0", "q1", "q2", "q3"},
+            alphabet={"0", "1"},
+            transfunc={
+                "q0": {"": "q2", "0": "q1"},
+                "q1": {"1": frozenset({"q1", "q3"})},
+                "q2": {"": "q1", "0": "q3"},
+                "q3": {"0": "q2"},
+            },
+            initial="q0",
+            accepting={"q2", "q3"},
+        ),
+        NFA(
+            states={"q0", "q1", "q2", "q3"},
+            alphabet={"a", "b"},
+            transfunc={
+                "q0": {"": "q1"},
+                "q1": {"a": frozenset({"q1", "q2"}), "b": "q2"},
+                "q2": {"a": frozenset({"q0", "q2"}), "b": "q3"},
+                "q3": {"b": "q1"},
+            },
+            initial="q0",
+            accepting={"q0"},
+        ),
+        NFA(
+            states={"q0", "q1", "q2"},
+            alphabet={"a", "b"},
+            transfunc={
+                "q0": {"": "q2", "b": "q1"},
+                "q1": {"a": frozenset({"q1", "q2"}), "b": "q2"},
+                "q2": {"a": "q0"},
+            },
+            initial="q0",
+            accepting={"q0"},
+        ),
+    ]
+    for nfa in nfas:
+        dfa = nfa.to_dfa()
+        pprint(dfa, width=100, compact=True)
+        print()
